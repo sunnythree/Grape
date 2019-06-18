e40759e5d088e4fd0691e06b912fffa8ad698160
sunnythree <1318288270@qq.com>
2019-06-18 09:08:22 +0800

    add Node shixian

--- src/javernn/node.cpp
+++ src/javernn/node.cpp
@@ -1,7 +1,27 @@
+#include <algorithm>
+#include "javernn/node.h"
+
 
 namespace javernn{
 
+    Node::Node(size_t in_size, size_t out_size) : prev_(in_size), next_(out_size) 
+    {
 
+    }
+    Node::~Node() 
+    {
 
+    }
 
+    size_t Node::PrevPort(const Edge &e) const {
+        auto it = std::find_if(prev_.begin(), prev_.end(),
+                                [&](edgeptr_t ep) { return ep.get() == &e; });
+        return (size_t)std::distance(prev_.begin(), it);
+    }
+
+    size_t Node::NextPort(const Edge &e) const {
+        auto it = std::find_if(next_.begin(), next_.end(),
+                                [&](edgeptr_t ep) { return ep.get() == &e; });
+        return (size_t)std::distance(next_.begin(), it);
+    }
 }
