6897d2a762bb61b4c4f4683c0a1aafb570b89af9
sunnythree <1318288270@qq.com>
2019-07-27 21:02:03 +0800

    fix cereal optional

--- include/grape/params/op_params.h
+++ include/grape/params/op_params.h
@@ -24,14 +24,46 @@ namespace Grape{
         {
             ar(cereal::make_nvp("name_",name_));
             ar(cereal::make_nvp("type_",type_));
-            ar(cereal::make_nvp("batch_",batch_));
-            ar(cereal::make_nvp("in_dim_",in_dim_));
-            ar(cereal::make_nvp("out_dim_",out_dim_));
-            ar(cereal::make_nvp("data_path_",data_path_));
-            ar(cereal::make_nvp("label_path_",label_path_));
-            ar(cereal::make_nvp("has_bias_",has_bias_));
-            ar(cereal::make_nvp("random_",random_));
-            ar(cereal::make_nvp("sample_count_",sample_count_));
+            try{
+                ar(cereal::make_nvp("batch_",batch_));
+            }  catch(cereal::Exception&){
+                ar.setNextName(nullptr);
+            }
+            try{
+                ar(cereal::make_nvp("in_dim_",in_dim_));
+            }  catch(cereal::Exception&){
+                ar.setNextName(nullptr);
+            }
+            try{
+                ar(cereal::make_nvp("out_dim_",out_dim_));
+            }  catch(cereal::Exception&){
+                ar.setNextName(nullptr);
+            }
+            try{
+                ar(cereal::make_nvp("data_path_",data_path_));
+            }  catch(cereal::Exception&){
+                ar.setNextName(nullptr);
+            }
+            try{
+                ar(cereal::make_nvp("label_path_",label_path_));
+            }  catch(cereal::Exception&){
+                ar.setNextName(nullptr);
+            }
+            try{
+                ar(cereal::make_nvp("has_bias_",has_bias_));
+            }  catch(cereal::Exception&){
+                ar.setNextName(nullptr);
+            }
+            try{
+                ar(cereal::make_nvp("random_",random_));
+            }  catch(cereal::Exception&){
+                ar.setNextName(nullptr);
+            }
+            try{
+                ar(cereal::make_nvp("sample_count_",sample_count_));
+            }  catch(cereal::Exception&){
+                ar.setNextName(nullptr);
+            }
         }
     };
 }
